3ed5ed4e2a47c4af1163e12e9fa2baa0ed9ccd4f
md anas <anas2542wy@gmail.com>
2025-12-22 18:56:36 +0600

    updated code

--- src/hooks/useRole.jsx
+++ src/hooks/useRole.jsx
@@ -9,7 +9,7 @@ const useRole = () => {
 
   useEffect(() => {
     if (user?.email) {
-      axios.get(`http://localhost:5000/users/role/${user.email}`)
+      axios.get(`https://blood-donation-backentd-11.vercel.app//users/role/${user.email}`)
         .then(res => {
           setRole(res.data?.role);
           setRoleLoading(false);
